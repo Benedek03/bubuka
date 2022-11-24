fbbbd397e527bf1a86e0cc542ee8207141a3dbbe
Benedek <szankabg@gmail.com>
2022-11-24 14:25:53 +0100

    improve consistency

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ import { config } from "dotenv"; config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, async client => {
+client.once(Events.ClientReady, async (client) => {
     console.log(`Ready! Logged in as ${client.user.tag}`);
 
     const rest = new REST({ version: '10' }).setToken(process.env.TOKEN as string);
     try {
         console.log(`Started refreshing application (/) commands.`);
         const data = await rest.put(
-            process.env.GUILDID? Routes.applicationGuildCommands(process.env.APPLICATIONID as string, process.env.GUILDID as string) : Routes.applicationCommands(process.env.APPLICATIONID as string),
+            process.env.GUILDID ? Routes.applicationGuildCommands(process.env.APPLICATIONID as string, process.env.GUILDID as string) : Routes.applicationCommands(process.env.APPLICATIONID as string),
             { body: commandDataArray },
         );
         console.log(`Successfully reloaded application (/) commands.`);
